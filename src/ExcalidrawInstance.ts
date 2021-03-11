7a4bcfb614e2da6d5c48ad7388d1e3683096938c
David Nussio <david.nussio@gmail.com>
2021-03-11 08:36:30 +0100

    POC create-react-app with @excalidraw dep

--- src/ExcalidrawInstance.ts
+++ src/ExcalidrawInstance.ts
@@ -1,132 +1,154 @@
 import { Disposable, EventEmitter } from "vscode";
 
 export interface MessageStream {
-    registerMessageHandler(handler: (message: any) => void): Disposable;
-    sendMessage(message: any): void;
+  registerMessageHandler(handler: (message: any) => void): Disposable;
+  sendMessage(message: any): void;
 }
 
 export interface ExcalidrawEvent {
-    type: 'init' | 'autosave' | 'save' | 'export' | 'configure';
-    data: string;
-    actionId?: string;
+  type: "init" | "autosave" | "save" | "export" | "configure";
+  data: string;
+  actionId?: string;
 }
 
 export interface ExcalidrawAction {
-    type: 'load' | 'save' | 'deleteShape' | 'export';
-    data?: string;
-    autosave?: 1 | 0;
-    scale?: number,
+  type: "load" | "save" | "deleteShape" | "export";
+  data?: string;
+  autosave?: 1 | 0;
+  scale?: number;
 }
 
 export class ExcalidrawInstance implements Disposable {
-    private disposables: Disposable[] = [];
-    private readonly onInitEmitter = new EventEmitter<void>();
-    public readonly onInit = this.onInitEmitter.event;
-    private readonly onChangeEmitter = new EventEmitter<{ newData: string, oldData: string | undefined } | void>();
-    public readonly onChange = this.onChangeEmitter.event;
-    private readonly onSaveEmitter = new EventEmitter<string | void>();
-    public readonly onSave = this.onSaveEmitter.event;
+  private disposables: Disposable[] = [];
+  private readonly onInitEmitter = new EventEmitter<void>();
+  public readonly onInit = this.onInitEmitter.event;
+  private readonly onChangeEmitter = new EventEmitter<{
+    newData: string;
+    oldData: string | undefined;
+  } | void>();
+  public readonly onChange = this.onChangeEmitter.event;
+  private readonly onSaveEmitter = new EventEmitter<string | void>();
+  public readonly onSave = this.onSaveEmitter.event;
 
-    private currentData: string | undefined = undefined;
-    private currentActionId = 0;
-    private responseHandlers = new Map<string, { resolve: (response: ExcalidrawEvent) => void, reject: Function }>();
+  private currentData: string | undefined = undefined;
+  private currentActionId = 0;
+  private responseHandlers = new Map<
+    string,
+    { resolve: (response: ExcalidrawEvent) => void; reject: Function }
+  >();
 
-    constructor(public readonly messageStream: MessageStream) {
-        this.disposables.push(
-            messageStream.registerMessageHandler((msg) => this.handleEvent(msg as ExcalidrawEvent))
-        );
-    }
+  constructor(public readonly messageStream: MessageStream) {
+    this.disposables.push(
+      messageStream.registerMessageHandler((msg) =>
+        this.handleEvent(msg as ExcalidrawEvent)
+      )
+    );
+  }
 
-    private async handleEvent(event: ExcalidrawEvent) {
-        switch (event.type) {
-            case 'init':
-                this.onInitEmitter.fire();
-                break;
-            case 'autosave':
-                const newData = event.data;
-                const oldData = this.currentData;
-                this.currentData = newData;
-                this.onChangeEmitter.fire({
-                    newData,
-                    oldData,
-                });
-                break;
-            case 'save':
-                this.onSaveEmitter.fire();
-                break;
-            default:
-                // console.log(event);
-                break;
-        }
-
-        if ('actionId' in event && event.actionId) {
-            const responseHandler = this.responseHandlers.get(event.actionId);
-            this.responseHandlers.delete(event.actionId);
-            if (responseHandler) {
-                responseHandler.resolve(event);
-            }
-        }
-    }
-
-    private sendAction(action: ExcalidrawAction, expectResponse: boolean = false): Promise<ExcalidrawEvent> {
-        return new Promise((resolve, reject) => {
-            const actionId = `${this.currentActionId++}`;
-            if (expectResponse) {
-                this.responseHandlers.set(actionId, {
-                    resolve: response => resolve(response),
-                    reject,
-                });
-            }
-            this.messageStream.sendMessage(JSON.stringify({
-                ...action,
-                actionId,
-                source: 'vscode-excalidraw',
-            }));
-
-            if (!expectResponse) {
-                resolve();
-            }
+  private async handleEvent(event: ExcalidrawEvent) {
+    switch (event.type) {
+      case "init":
+        this.onInitEmitter.fire();
+        break;
+      case "autosave":
+        const newData = event.data;
+        const oldData = this.currentData;
+        this.currentData = newData;
+        this.onChangeEmitter.fire({
+          newData,
+          oldData,
         });
+        break;
+      case "save":
+        this.onSaveEmitter.fire();
+        break;
+      default:
+        // console.log(event);
+        break;
     }
 
-    public loadData(data: string) {
-        this.currentData = undefined;
-        this.sendAction({
-            type: 'load',
-            data,
-            autosave: 1,
+    if ("actionId" in event && event.actionId) {
+      const responseHandler = this.responseHandlers.get(event.actionId);
+      this.responseHandlers.delete(event.actionId);
+      if (responseHandler) {
+        responseHandler.resolve(event);
+      }
+    }
+  }
+
+  private sendAction(
+    action: ExcalidrawAction,
+    expectResponse: boolean = false
+  ): Promise<ExcalidrawEvent> {
+    return new Promise((resolve, reject) => {
+      const actionId = `${this.currentActionId++}`;
+      if (expectResponse) {
+        this.responseHandlers.set(actionId, {
+          resolve: (response) => resolve(response),
+          reject,
         });
-    }
+      }
+      this.messageStream.sendMessage(
+        JSON.stringify({
+          ...action,
+          actionId,
+          source: "vscode-excalidraw",
+        })
+      );
 
-    public async getData(type: 'raw' | 'svg' | 'png' = 'raw') {
-        const message = await this.sendAction({
-            type: 'save',
-            data: type,
-        }, true);
-        return message.data;
-    }
+      if (!expectResponse) {
+        resolve({} as ExcalidrawEvent);
+      }
+    });
+  }
 
-    public async deleteShape() {
-        await this.sendAction({
-            type: 'deleteShape',
-        }, true);
-    }
+  public loadData(data: string) {
+    this.currentData = undefined;
+    this.sendAction({
+      type: "load",
+      data,
+      autosave: 1,
+    });
+  }
 
-    public async exportTo(format: string, scale = 1) {
-        const res = await this.sendAction({
-            type: 'save',
-            data: format,
-            scale,
-        }, true);
+  public async getData(type: "raw" | "svg" | "png" = "raw") {
+    const message = await this.sendAction(
+      {
+        type: "save",
+        data: type,
+      },
+      true
+    );
+    return message.data;
+  }
 
-        return res.data;
-    }
+  public async deleteShape() {
+    await this.sendAction(
+      {
+        type: "deleteShape",
+      },
+      true
+    );
+  }
 
-    dispose() {
-        this.onInitEmitter.dispose();
-        this.onChangeEmitter.dispose();
-        this.onSaveEmitter.dispose();
-        this.disposables.forEach(d => d.dispose());
-        this.disposables = [];
-    }
+  public async exportTo(format: string, scale = 1) {
+    const res = await this.sendAction(
+      {
+        type: "save",
+        data: format,
+        scale,
+      },
+      true
+    );
+
+    return res.data;
+  }
+
+  dispose() {
+    this.onInitEmitter.dispose();
+    this.onChangeEmitter.dispose();
+    this.onSaveEmitter.dispose();
+    this.disposables.forEach((d) => d.dispose());
+    this.disposables = [];
+  }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,232 +1,278 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as path from 'path';
-import * as http from 'http';
-import * as fs from 'fs';
+import * as path from "path";
+import * as http from "http";
+import * as fs from "fs";
 
-import * as serverStatic from 'serve-static';
+import * as serverStatic from "serve-static";
 import * as finalhandler from "finalhandler";
 
-import * as vscode from 'vscode';
-import { AddressInfo } from 'net';
+import * as vscode from "vscode";
+import { AddressInfo } from "net";
 
-import { ExcalidrawInstance } from './ExcalidrawInstance';
+import { ExcalidrawInstance } from "./ExcalidrawInstance";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
 function startServer(): http.Server {
-	const root = path.resolve(__dirname, '../build');
-	const serve = serverStatic(root);
-	return http.createServer(function (req, res) {
-		serve(req as any, res as any, finalhandler(req, res));
-	});
+  const root = path.resolve(__dirname, "../build");
+  const serve = serverStatic(root);
+  return http.createServer(function (req, res) {
+    serve(req as any, res as any, finalhandler(req, res));
+  });
 }
 
 function setupWebview(webview: vscode.Webview, port: number) {
-	webview.options = {
-		enableScripts: true,
-	};
-	webview.html = `<!DOCTYPE html><html>
-	<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="Content-Security-Policy" content="default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src * 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src *; style-src * 'unsafe-inline'; worker-src * data: 'unsafe-inline' 'unsafe-eval'; font-src * 'unsafe-inline' 'unsafe-eval';">
-	<style>
-		html { height: 100%; width: 100%; padding: 0; margin: 0; }
-		body { height: 100%; width: 100%; padding: 0; margin: 0; }
-		iframe { height: 100%; width: 100%; padding: 0; margin: 0; border: 0; display: block; }
-	</style>
-	</head>
-	<body>
-		<iframe src="http://localhost:${port}/index.html?embed=1"></iframe>
-		<script>
-			const api = window.VsCodeApi = acquireVsCodeApi();
+  webview.options = {
+    enableScripts: true,
+  };
+  webview.html = `<!DOCTYPE html><html>
+  	<head>
+  	<meta charset="UTF-8">
+  	<meta http-equiv="Content-Security-Policy" content="default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src * 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src *; style-src * 'unsafe-inline'; worker-src * data: 'unsafe-inline' 'unsafe-eval'; font-src * 'unsafe-inline' 'unsafe-eval';">
+  	<style>
+  		html { height: 100%; width: 100%; padding: 0; margin: 0; }
+  		body { height: 100%; width: 100%; padding: 0; margin: 0; }
+  		iframe { height: 100%; width: 100%; padding: 0; margin: 0; border: 0; display: block; }
+  	</style>
+  	</head>
+  	<body>
+  		<iframe src="http://localhost:${port}/index.html?embed=1"></iframe>
+  		<script>
+  			const api = window.VsCodeApi = acquireVsCodeApi();
 
-			window.addEventListener('message', event => {
-				if (event.source === window.frames[0]) {
-					api.postMessage(event.data);
-				} else {
-					window.frames[0].postMessage(event.data, 'http://localhost:${port}');
-				}
-			});
-		</script>
-	</body>
-	</html>`;
-	return new ExcalidrawInstance({
-		sendMessage: (msg) => webview.postMessage(msg),
-		registerMessageHandler: (handler) => webview.onDidReceiveMessage(handler),
-	});
+  			window.addEventListener('message', event => {
+  				console.log('######## MESSAGE ');
+  				if (event.source === window.frames[0]) {
+  					console.log(' ------ to vscode ');
+  					api.postMessage(event.data);
+  				} else { //if (event.data.source === "vscode-excalidraw") {
+  					console.log(' ------ to iframe ',typeof event.data);
+  					window.frames[0].postMessage(event.data, '*');
+  				}
+  			});
+  		</script>
+  	</body>
+  	</html>`;
+  return new ExcalidrawInstance({
+    sendMessage: (msg) => webview.postMessage(msg),
+    registerMessageHandler: (handler) => webview.onDidReceiveMessage(handler),
+  });
 }
 
-class ExcalidrawEditorProvider implements vscode.CustomTextEditorProvider, vscode.Disposable {
-	server: http.Server;
-	serverReady: Promise<void>;
-	instances: { excalidrawInstance: ExcalidrawInstance, panel: vscode.WebviewPanel }[] = [];
+class ExcalidrawEditorProvider
+  implements vscode.CustomTextEditorProvider, vscode.Disposable {
+  server: http.Server;
+  serverReady: Promise<void>;
 
-	constructor(private context: vscode.ExtensionContext) {
-		this.server = startServer();
-		this.serverReady = new Promise(resolve => {
-			this.server.listen(undefined, 'localhost', () => {
-				resolve();
-			});
-		});
-	}
+  instances: {
+    excalidrawInstance: ExcalidrawInstance;
+    panel: vscode.WebviewPanel;
+  }[] = [];
 
-	async resolveCustomTextEditor(document: vscode.TextDocument, webviewPanel: vscode.WebviewPanel, token: vscode.CancellationToken) {
-		let localDisposables: vscode.Disposable[] = [];
-		let initialized = false;
-		let firstChange = false;
-		await this.serverReady;
-		const excalidrawInstance = setupWebview(webviewPanel.webview, (this.server.address() as AddressInfo).port);
-		let isEditorSaving = false;
+  constructor(private context: vscode.ExtensionContext) {
+    this.server = startServer();
+    this.serverReady = new Promise((resolve) => {
+      this.server.listen(undefined, "localhost", () => {
+        resolve();
+      });
+    });
+  }
 
-		localDisposables.push(vscode.workspace.onDidChangeTextDocument((event) => {
-			if (event.document !== document || isEditorSaving || event.contentChanges.length === 0) {
-				return;
-			}
+  async resolveCustomTextEditor(
+    document: vscode.TextDocument,
+    webviewPanel: vscode.WebviewPanel,
+    token: vscode.CancellationToken
+  ) {
+    let localDisposables: vscode.Disposable[] = [];
+    let initialized = false;
+    let firstChange = false;
+    // await this.serverReady;
+    const excalidrawInstance = setupWebview(
+      webviewPanel.webview,
+      3000 //(this.server.address() as AddressInfo).port
+    );
+    let isEditorSaving = false;
 
-			excalidrawInstance.loadData(event.document.getText());
-		}));
+    localDisposables.push(
+      vscode.workspace.onDidChangeTextDocument((event) => {
+        if (
+          event.document !== document ||
+          isEditorSaving ||
+          event.contentChanges.length === 0
+        ) {
+          return;
+        }
 
-		localDisposables.push(excalidrawInstance.onChange(async (data) => {
-			if (!firstChange) {
-				firstChange = true;
-				return;
-			}
+        excalidrawInstance.loadData(event.document.getText());
+      })
+    );
 
-			if (!data || data.newData === document.getText()) {
-				return;
-			}
+    localDisposables.push(
+      excalidrawInstance.onChange(async (data) => {
+        if (!firstChange) {
+          firstChange = true;
+          return;
+        }
 
-			const edit = new vscode.WorkspaceEdit();
-			edit.replace(document.uri, new vscode.Range(0, 0, document.lineCount, 0), data.newData);
-			isEditorSaving = true;
-			try {
-				await vscode.workspace.applyEdit(edit);
-			} finally {
-				isEditorSaving = false;
-			}
-		}));
+        if (!data || data.newData === document.getText()) {
+          return;
+        }
 
-		localDisposables.push(
-			excalidrawInstance.onSave(async () => {
-				await document.save();
-			})
-		);
-		webviewPanel.onDidDispose(() => {
-			localDisposables.forEach(d => d.dispose());
-			localDisposables = [];
-			this.instances = this.instances.filter(i => i.panel !== webviewPanel);
-		});
+        const edit = new vscode.WorkspaceEdit();
+        edit.replace(
+          document.uri,
+          new vscode.Range(0, 0, document.lineCount, 0),
+          data.newData
+        );
+        isEditorSaving = true;
+        try {
+          await vscode.workspace.applyEdit(edit);
+        } finally {
+          isEditorSaving = false;
+        }
+      })
+    );
 
-		excalidrawInstance.onInit(() => {
-			if (initialized) {
-				return;
-			}
-			initialized = true;
-			excalidrawInstance.loadData(document.getText());
-		});
+    localDisposables.push(
+      excalidrawInstance.onSave(async () => {
+        await document.save();
+      })
+    );
+    webviewPanel.onDidDispose(() => {
+      localDisposables.forEach((d) => d.dispose());
+      localDisposables = [];
+      this.instances = this.instances.filter((i) => i.panel !== webviewPanel);
+    });
 
-		this.instances.push({
-			excalidrawInstance,
-			panel: webviewPanel,
-		});
-	}
+    excalidrawInstance.onInit(() => {
+      if (initialized) {
+        return;
+      }
+      initialized = true;
+      excalidrawInstance.loadData(document.getText());
+    });
 
-	broadcastDelete() {
-		this.instances.filter(i => i.panel.active).forEach(({ excalidrawInstance }) => {
-			excalidrawInstance.deleteShape();
-		});
-	}
+    this.instances.push({
+      excalidrawInstance,
+      panel: webviewPanel,
+    });
+  }
 
-	public async exportTo(format: 'png' | 'svg' = 'png', scale = 1.0): Promise<string | null> {
-		const instance = this.instances.find(i => i.panel.active);
+  broadcastDelete() {
+    this.instances
+      .filter((i) => i.panel.active)
+      .forEach(({ excalidrawInstance }) => {
+        excalidrawInstance.deleteShape();
+      });
+  }
 
-		if (!instance) {
-			return null;
-		}
+  public async exportTo(
+    format: "png" | "svg" = "png",
+    scale = 1.0
+  ): Promise<string | null> {
+    const instance = this.instances.find((i) => i.panel.active);
 
-		const { excalidrawInstance } = instance;
-		return await excalidrawInstance.exportTo(format, scale);
-	}
+    if (!instance) {
+      return null;
+    }
 
-	dispose() {
-		if (this.server) {
-			this.server.close();
-		}
+    const { excalidrawInstance } = instance;
+    return await excalidrawInstance.exportTo(format, scale);
+  }
 
-		this.instances = [];
-	}
+  dispose() {
+    if (this.server) {
+      this.server.close();
+    }
+
+    this.instances = [];
+  }
 }
 
 export async function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	const provider = new ExcalidrawEditorProvider(context);
-	context.subscriptions.push(vscode.window.registerCustomEditorProvider(
-		'brijeshb42-excalidraw.texteditor',
-		provider,
-		{ webviewOptions: { retainContextWhenHidden: true } },
-	));
-	vscode.commands.registerCommand('brijeshb42-excalidraw.deleteshape', async () => {
-		provider.broadcastDelete();
-	});
-	vscode.commands.registerCommand('brijeshb42-excalidraw.export', async () => {
-		const result = await vscode.window.showQuickPick(['png', 'svg'], {
-			placeHolder: 'Select the export format',
-			ignoreFocusOut: false,
-		});
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  const provider = new ExcalidrawEditorProvider(context);
+  context.subscriptions.push(
+    vscode.window.registerCustomEditorProvider(
+      "brijeshb42-excalidraw.texteditor",
+      provider,
+      { webviewOptions: { retainContextWhenHidden: true } }
+    )
+  );
+  vscode.commands.registerCommand(
+    "brijeshb42-excalidraw.deleteshape",
+    async () => {
+      provider.broadcastDelete();
+    }
+  );
+  vscode.commands.registerCommand("brijeshb42-excalidraw.export", async () => {
+    const result = await vscode.window.showQuickPick(["png", "svg"], {
+      placeHolder: "Select the export format",
+      ignoreFocusOut: false,
+    });
 
-		if (!result) {
-			return;
-		}
+    if (!result) {
+      return;
+    }
 
-		let scale: string | number | undefined = 1;
+    let scale: string | number | undefined = 1;
 
-		if (result === 'png') {
-			scale = await vscode.window.showInputBox({
-				value: '1',
-				prompt: 'Input the scale of image (any number)',
-				placeHolder: '1',
-				ignoreFocusOut: false,
-				validateInput(value) {
-					if (!Number.isNaN(parseFloat(value))) {
-						return null;
-					}
+    if (result === "png") {
+      scale = await vscode.window.showInputBox({
+        value: "1",
+        prompt: "Input the scale of image (any number)",
+        placeHolder: "1",
+        ignoreFocusOut: false,
+        validateInput(value) {
+          if (!Number.isNaN(parseFloat(value))) {
+            return null;
+          }
 
-					return 'error';
-				},
-			});
-		}
+          return "error";
+        },
+      });
+    }
 
-		if (!scale) {
-			return;
-		}
+    if (!scale) {
+      return;
+    }
 
-		const data = await provider.exportTo(result as 'svg' | 'png', parseFloat(scale as string));
+    const data = await provider.exportTo(
+      result as "svg" | "png",
+      parseFloat(scale as string)
+    );
 
-		if (!data) {
-			vscode.window.showErrorMessage('Could not export.');
-		} else {
-			vscode.window.showSaveDialog({
-				saveLabel: `Export as ${result}`,
-			}).then(uri => {
-				if (!uri) {
-					return;
-				}
+    if (!data) {
+      vscode.window.showErrorMessage("Could not export.");
+    } else {
+      vscode.window
+        .showSaveDialog({
+          saveLabel: `Export as ${result}`,
+        })
+        .then((uri) => {
+          if (!uri) {
+            return;
+          }
 
-				let modUri = uri.path;
-				if (!modUri.endsWith(`.${result}`)) {
-					modUri += `.${result}`;
-				}
+          let modUri = uri.path;
+          if (!modUri.endsWith(`.${result}`)) {
+            modUri += `.${result}`;
+          }
 
-				if (result === 'png') {
-					fs.writeFileSync(modUri, data.replace(/^data:image\/\w+;base64,/, ''), { encoding: 'base64' });
-				} else {
-					fs.writeFileSync(modUri, data);
-				}
-			});
-		}
-	});
-	context.subscriptions.push(provider);
+          if (result === "png") {
+            fs.writeFileSync(
+              modUri,
+              data.replace(/^data:image\/\w+;base64,/, ""),
+              { encoding: "base64" }
+            );
+          } else {
+            fs.writeFileSync(modUri, data);
+          }
+        });
+    }
+  });
+  context.subscriptions.push(provider);
 }
